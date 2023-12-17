9db6e93f9cca40554633727dd446d84ab746cab3
thomas0114 <thomas.k98114@gmail.com>
2023-12-17 17:14:41 -0500

    updated solana

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -16,23 +16,23 @@ const Home = () => {
         <TextWelcome>
           Welcome to Willow:{"\u00a0"}
           <TextWelcomeSmall>
-            A New Era of Playful Innovation on Binance Smart Chain
+            A New Era of Playful Innovation on Solana Chain
           </TextWelcomeSmall>
         </TextWelcome>
         <TextWelcomeDescription>
           Willow is a cryptocurrency that embrace the playful spirit of meme
-          culture and gamification on Binance Smart Chain. Willow is more than
+          culture and gamification on Solana Chain. Willow is more than
           just a cryptocurrency—it's an experience, a journey, and a celebration
           of the vibrant and creative community that forms its backbone.
           Inspired by the charm of meme doges and fueled by the efficiency of
           blockchain technology, Willow represents a unique fusion of fun and
           financial innovation.
         </TextWelcomeDescription>
-        <TextWelcomeDescription>
+        <TextWelcomeDescription>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
           Contract Address:{" "}
           <span>
             <TextContractAddress
-              href={`https://bscscan.com/address/${process.env.REACT_APP_ADDRESS_TOKEN_CONTRACT}`}
+              href={`https://solscan.io/account/${process.env.REACT_APP_ADDRESS_TOKEN_CONTRACT}`}
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -76,7 +76,7 @@ const Home = () => {
               <ButtonViewExplorer
                 onClick={() => {
                   window.open(
-                    `https://etherscan.io/address/${process.env.REACT_APP_ADDRESS_TOKEN_CONTRACT}`
+                    `https://solscan.io/account/${process.env.REACT_APP_ADDRESS_TOKEN_CONTRACT}`
                   );
                 }}
               >
@@ -107,7 +107,7 @@ const Home = () => {
         <MobileButtonViewExplorer
           onClick={() => {
             window.open(
-              `https://etherscan.io/address/${process.env.REACT_APP_ADDRESS_TOKEN_CONTRACT}`
+              `https://solscan.io/account/${process.env.REACT_APP_ADDRESS_TOKEN_CONTRACT}`
             );
           }}
         >
